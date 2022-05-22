a0bf695e28601b533291fe8f40907c9d904ff2b0
tony dugué <duguetony@gmail.com>
2022-05-22 21:05:09 +0200

    initialization client, cleanup & add theme starter

---
+++ src/shared/utils/theme.ts
@@ -0,0 +1,15 @@
+import {createTheme} from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF9900',
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#OOOOOO',
+      contrastText: '#FFFFFF'
+    }
+  }
+})
+
