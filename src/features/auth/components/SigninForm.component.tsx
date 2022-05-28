5a508e736d8506ebd0887c1e21bc791934b71d34
tony dugué <duguetony@gmail.com>
2022-05-28 21:00:16 +0200

    signin => add logic in component for login when submit form home page => add link for test logout

--- src/features/auth/components/SigninForm.component.tsx
+++ src/features/auth/components/SigninForm.component.tsx
@@ -1,11 +1,23 @@
-import { FC, FormEvent } from 'react'
-import { Box, Grid, TextField, InputLabel, Typography, Button, Divider } from "@mui/material";
-import {Link} from "react-router-dom";
+import {FC, FormEvent, useEffect} from 'react'
+import {Box, Grid, TextField, InputLabel, Typography, Button, Divider, CircularProgress} from "@mui/material";
+import {Link, useNavigate} from "react-router-dom";
+
 import useInput from "../../../hooks/input/use-inputs";
 import {validateEmail} from "../../../shared/utils/validation/email";
 import {validatePasswordLength} from "../../../shared/utils/validation/length";
 
+// Redux
+import {useAppDispatch, useAppSelector} from "../../../hooks/redux/hook";
+import {login, reset} from "../authSlice";
+import {LoginUser} from "../models/LoginUser.interface";
+
 const SigninFormComponent: FC = () => {
+
+  const dispatch = useAppDispatch();
+
+  const { isLoading, isSuccess, isAuthenticated } = useAppSelector( (state) => state.auth);
+
+  const navigate = useNavigate();
 
   /* ========== Fields Validation ============ */
 
@@ -38,10 +50,25 @@ const SigninFormComponent: FC = () => {
     if (emailHasError || passwordHasError) return;
     if (email.length === 0 || password.length === 0) return;
 
-    console.log('USER', email, password)
+    const loginUser: LoginUser = { email, password }
 
-    clearForm()
+    dispatch(login(loginUser))
   }
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(reset())
+      clearForm()
+      navigate('/signin')
+    }
+  }, [isSuccess, dispatch])
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+    navigate('/')
+  }, [isAuthenticated])
+
+  if (isLoading) return <CircularProgress sx={{ marginTop: '64px' }} color="primary" />
 
   return (
     <>
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,7 +1,26 @@
+import {useAppDispatch, useAppSelector} from "../hooks/redux/hook";
+import {logout} from "../features/auth/authSlice";
+
 const HomePage = () => {
+
+  const dispatch = useAppDispatch();
+
+  const { user } = useAppSelector( (state) => state.auth);
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
    <div>
-    HomePage
+     <h1>Home Page</h1>
+
+     <a
+       onClick={logoutHandler}
+       style={{ backgroundColor: 'yellow', cursor: 'pointer', height: '40px', width: '60px', padding: '8px' }}
+     >Logout</a>
+
+     {user?.email}
    </div>
   )
 }
