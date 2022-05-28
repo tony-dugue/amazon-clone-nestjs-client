bf6cd523a033a3340d967b72a11416eddffdf1b8
tony dugué <duguetony@gmail.com>
2022-05-28 11:48:11 +0200

    configuration Redux Toolkit, add store & auth slice + register request

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,71 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {DisplayUser} from "./models/DisplayUser.interface";
+import {Jwt} from "./models/Jwt";
+import {NewUser} from "./models/NewUser";
+import authService from "./services/auth.service";
+
+// TODO: move higher in global => refactorisation
+interface AsyncState {
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+}
+
+interface AuthState extends AsyncState {
+  user?: DisplayUser | null;
+  jwt?: Jwt;
+  isAuthenticated?: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,  // user
+  jwt: null,   // jwt
+  isAuthenticated: false,
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+}
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user: NewUser, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      return thunkAPI.rejectWithValue('la création du compte a échouée')
+    }
+  }
+)
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+    }
+  },
+
+  extraReducers: (builder => {
+    builder
+    // Register
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.user = null;
+      })
+  })
+})
+
+export default authSlice.reducer;
---
+++ src/features/auth/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { DisplayUser } from "../models/DisplayUser.interface";
+import {NewUser} from "../models/NewUser";
+import axios from "axios";
+
+const register = async (newUser: NewUser): Promise<DisplayUser | null> => {
+  const response = await axios.post(`${process.env.REACT_APP_BASE_API}/auth/register`, newUser)
+  return response.data;
+}
+
+const authService = {
+  register: register,
+  // login,
+  // logout,
+  // verifyJwt,
+}
+
+export default authService;
