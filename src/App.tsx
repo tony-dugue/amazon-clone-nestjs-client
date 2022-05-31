b2a6e2e10e49af615a2e7993b124609a5bdc4a7f
tony dugué <duguetony@gmail.com>
2022-05-31 22:47:31 +0200

    add Cart page with Header

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import {ThemeProvider} from "@mui/material";
 import {theme} from "./shared/utils/theme";
 
+import PrivateRoute from "./features/auth/components/PrivateRoute";
+
 import HomePage from './pages/Home.page';
 import RegisterPage from './pages/Register.page';
 import SigninPage from './pages/Signin.page';
-import PrivateRoute from "./features/auth/components/PrivateRoute";
+import CartPage from "./pages/Cart.page";
 
 import { store } from './store'
 
@@ -18,6 +20,8 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<PrivateRoute page={<HomePage />} />} />
+          <Route path='/cart' element={<PrivateRoute page={<CartPage />} />} />
+
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/signin' element={<SigninPage />} />
           <Route path='*' element={<Navigate to='/' />} />
---
+++ src/pages/Cart.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderComponent from "../features/products/components/Header.component";
+
+const CartPage = () => {
+  return (
+   <div>
+     <HeaderComponent />
+   </div>
+  )
+}
+
+export default CartPage
